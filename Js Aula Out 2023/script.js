3121fa16a7981ef21827df56806958d7afde2afe
Guilherme Sousa <123672664+uguisousa@users.noreply.github.com>
2024-04-02 16:54:53 -0300

    Add files via upload

---
+++ Js Aula Out 2023/script.js
@@ -0,0 +1,18 @@
+const div1 = document.querySelector("#div1");
+div1.innerHTML="Será <b>que</b> funciona?";
+
+const h1 = document.querySelector("h1");
+h1.innerHTML=" - Aula 9 "+h1.innerHTML;
+h1.style.color="green";
+h1.style.backgroundColor="red";
+
+const p = document.querySelectorAll("p");
+p[0].innerHTML="eita!";
+p[1].innerHTML="eita!";
+p[2].innerHTML="eita!";
+
+p[0].style.backgroundColor="blue";
+p[0]
+
+
+
