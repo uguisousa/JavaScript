c45b6a28bc7d94d167e65dffc6757460c6bf7ec9
uguisousa <guilhermehssousa872@gmail.com>
2024-05-22 13:49:39 -0300

    nv eps

---
+++ introducao-javascript-2/js/principal.js
@@ -0,0 +1,8 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("tr");
+
+
+console.log(paciente); // Verifica se o paciente está sendo selecionado corretamente
+
